cbef6d977b209897556ebcaa9bf66918f18fa617
makimovsky <makimofficial@gmail.com>
2024-10-21 18:33:23 +0200

    deleted filters in django

--- taskpilot_app/views.py
+++ taskpilot_app/views.py
@@ -1,8 +1,6 @@
 from taskpilot_app import serializers
 from taskpilot_app import models
 from rest_framework import viewsets
-from rest_framework import filters
-from django.shortcuts import render
 
 
 class WorkersViewSet(viewsets.ModelViewSet):
@@ -18,15 +16,11 @@ class ClientsViewSet(viewsets.ModelViewSet):
 class ProjectsViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProjectsSerializer
     queryset = models.Projects.objects.all()
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name', 'description')
 
 
 class TasksViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TasksSerializer
     queryset = models.Tasks.objects.all()
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name', 'description')
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
